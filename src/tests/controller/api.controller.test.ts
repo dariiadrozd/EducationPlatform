fdc02967a38584621d0f31354f91ce18b9dfa350
dariiadrozd <darriiadrozd@gmail.com>
2024-01-09 11:31:20 +0100

    start

--- src/tests/controller/api.controller.test.ts
+++ src/tests/controller/api.controller.test.ts
@@ -1,10 +1,25 @@
 import request from "supertest";
-import app from '../../app'
-
+import app from '../../app';
 
 test('reg', async () => {
+  try {
     const res = await request(app).post('/api').send({ name: 'Darya', surname: 'Drozd', email: 'drozd.d.rus@gmail.com', pwd: '123852' });
 
-    expect(res.statusCode).toBe(200)
+    expect(res.statusCode).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
-  });
+  } catch (error: any) {
+    expect(error.message).toBeTruthy();
+  }
+});
+
+test('auth', async () => {
+  try {
+    const res = await request(app).post('/api').send({ email: 'drozd.d.rus@gmail.com', pwd: '123852' });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  } catch (error: any) {
+    expect(error.message).toBeTruthy();
+  }
+
+})
