c20965b1c0efe62068e0de1add23f1896e45efe8
dariiadrozd <darriiadrozd@gmail.com>
2024-01-27 21:15:42 +0100

    start

---
+++ Client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header"
+
+function HomePage() {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
+
+export default HomePage;
